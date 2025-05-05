270eea8d6443278a36c98e169cf494db7c05a9ba
sentriz <senan@senan.xyz>
2025-05-05 22:21:15 +0100

    check negative score

--- tagmap/tagmap_test.go
+++ tagmap/tagmap_test.go
@@ -92,6 +92,37 @@ func TestDiffIgnoreMissing(t *testing.T) {
 	assert.InEpsilon(t, 100.0, score, 0)
 }
 
+// https://github.com/sentriz/wrtag/issues/99
+func TestNegativeScore(t *testing.T) {
+	t.Parallel()
+
+	var score float64
+	diff := Differ(TagWeights{}, &score)
+
+	diff("release", "Moon Boots", "Moon Boots")
+	diff("artist", "Bird Bear Hare and Fish", "BBHF")
+	diff("label", "", "SME Records")
+	diff("catalogue num", "", "SECL-2324")
+	diff("upc", "", "4547366368383")
+	diff("media format", "", "CD")
+
+	diff("track 1", "Bird Bear Hare and Fish – ウクライナ", "BBHF – ウクライナ")
+	diff("track 2", "Bird Bear Hare and Fish – ライカ", "BBHF – ライカ")
+	diff("track 3", "Bird Bear Hare and Fish – ダッシュボード", "BBHF – ダッシュボード")
+	diff("track 4", "Bird Bear Hare and Fish – レプリカント", "BBHF – レプリカント")
+	diff("track 5", "Bird Bear Hare and Fish – Hearts", "BBHF – Hearts")
+	diff("track 6", "Bird Bear Hare and Fish – 夏の光", "BBHF – 夏の光")
+	diff("track 7", "Bird Bear Hare and Fish – ページ", "BBHF – ページ")
+	diff("track 8", "Bird Bear Hare and Fish – Wake Up", "BBHF – Wake Up")
+	diff("track 9", "Bird Bear Hare and Fish – Different", "BBHF – Different")
+	diff("track 10", "Bird Bear Hare and Fish – 骨の音", "BBHF – 骨の音")
+	diff("track 11", "Bird Bear Hare and Fish – 次の火", "BBHF – 次の火")
+	diff("track 12", "Bird Bear Hare and Fish – Work", "BBHF – Work")
+
+	// probably we can come up with a better algorithm here to not produce a negative score
+	assert.InEpsilon(t, -63, score, 1)
+}
+
 func TestNorm(t *testing.T) {
 	t.Parallel()
 
