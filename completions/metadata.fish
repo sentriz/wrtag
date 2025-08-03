ad709586fe7de892e21d49addbfc3e6ea1c50640
quantenzitrone <general@dev.quantenzitrone.eu>
2025-08-03 08:26:16 +0200

    fish completions for metadata tool

---
+++ completions/metadata.fish
@@ -0,0 +1,51 @@
+#!/usr/bin/env 
+
+function __last_argument_from
+    string match -- (commandline -pcx)[-1] $argv
+end
+
+set commands read write clear
+set tags album artists title genres
+
+# don't suggest files if we haven't seen the end of the options
+complete -c metadata -n "not __fish_seen_subcommand_from --" --no-files
+
+# complete global options if we haven't seen a subcommand
+complete -c metadata -n "not __fish_seen_subcommand_from $commands" \
+    -o h -o help -l help -d "print help"
+
+complete -c metadata -n "not __fish_seen_subcommand_from $commands" \
+    -o log-level -l log-level -x -d "Set the logging level (default INFO)" \
+    -a "INFO WARN DEBUG ERROR"
+
+# complete subcommands if we haven't seen a subcommand
+complete -c metadata -n "not __fish_seen_subcommand_from $commands" \
+    -a read -d "read tags"
+
+complete -c metadata -n "not __fish_seen_subcommand_from $commands" \
+    -a write -d "write tags"
+
+complete -c metadata -n "not __fish_seen_subcommand_from $commands" \
+    -a clear -d "clear tags"
+
+# complete subcommand options
+complete -c metadata -n "__fish_seen_subcommand_from read" \
+    -n "not __fish_seen_subcommand_from - --" \
+    -o properties -l properties -d "Read file properties like length and bitrate"
+
+complete -c metadata -n "__fish_seen_subcommand_from $commands" \
+    -n "not __fish_seen_subcommand_from - --" \
+    -a "$tags"
+
+complete -c metadata -n "__fish_seen_subcommand_from write" \
+    -n "not __fish_seen_subcommand_from - --" \
+    -n "not __last_argument_from write ," \
+    -a "," -d "delimiter for different tags to write"
+
+complete -c metadata -n "__fish_seen_subcommand_from $commands" \
+    -n "not __fish_seen_subcommand_from - --" \
+    -a - -d "read list of paths from stdin"
+
+complete -c metadata -n "__fish_seen_subcommand_from $commands" \
+    -n "not __fish_seen_subcommand_from - --" \
+    -a -- -d "end of options, start of filenames"
