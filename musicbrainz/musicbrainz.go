12809e31051b1c88acb7c2527c94c403f9d02b1f
sentriz <senan@senan.xyz>
2025-08-18 13:17:36 +0100

    refactor(musicbrainz): simplify `inc` param

--- musicbrainz/musicbrainz.go
+++ musicbrainz/musicbrainz.go
@@ -31,7 +31,7 @@ type MBClient struct {
 func (c *MBClient) GetRelease(ctx context.Context, mbid string) (*Release, error) {
 	urlV := url.Values{}
 	urlV.Set("fmt", "json")
-	urlV.Set("inc", "recordings+artist-credits+labels+release-groups+genres+aliases+recording-level-rels+artist-rels+isrcs")
+	urlV.Set("inc", "recordings artist-credits labels release-groups genres aliases recording-level-rels artist-rels isrcs")
 
 	url, _ := url.Parse(joinPath(c.BaseURL, "release", mbid))
 	url.RawQuery = urlV.Encode()
